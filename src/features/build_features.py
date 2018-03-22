cc1073fa44c439022e2c4a15869f49f180280c1b
1512279 <1512279@brunel.ac.uk>
2018-03-22 09:19:03 +0000

    Finish with Association Rule Mining

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,10 +3,18 @@ from apyori import apriori
 if __name__ == "__main__":
 
     transactions = [
+        ['beer', 'sugar'],
         ['beer', 'nuts'],
+        ['ham', 'nuts', 'sugar'],
         ['beer', 'cheese'],
+        ['cheese', 'ham'],
+        ['beer', 'butter'],
     ]
 
-    results = list(apriori(transactions))
+    results = list(
+        apriori(transactions, min_support=1 / len(transactions), min_confidence=0.5, min_lift=1 * len(transactions) / 4,
+                max_length=3))
 
-    print(results)
+    for result in results:
+        if len(list(result)[0]) > 2:
+            print(list(list(result)[0]))
